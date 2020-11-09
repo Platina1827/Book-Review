64a37bff174989029b34c15851e8956348d02fcf
Platina1827 <44928685+Platina1827@users.noreply.github.com>
2020-11-08 17:31:56 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// Hambruger Menu toggle code. Source in CSS
+$(".toggle").click(function() {
+    $('header').toggleClass('show-menu');
+});
+
+/*Smooth Scrolling Source: https://youtu.be/y9nlfqT4s9s 
+ Code Source: https://github.com/cferdinandi/smooth-scroll*/
+
+ const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 800
+});
+
+
+//getting modal opening buttons
+var modalBtns = document.querySelectorAll(".modal-open")
+
+modalBtns.forEach(function(btn) {
+    btn.onclick = function() {
+        var modal = btn.getAttribute("data-modal");
+
+        document.getElementById(modal).style.display = "block";
+    };
+});
+
+//getting modal closing buttons
+var closeBtns = document.querySelectorAll(".modal-close");
+
+closeBtns.forEach(function(btn) {
+    btn.onclick = function() {
+        var modal = (btn.closest(".modal").style.display = "none");
+    };
+});
+
+window.onclick = function(e) {
+    if (e.target.className === "modal") {
+        e.target.style.display = "none";
+    }
+};
